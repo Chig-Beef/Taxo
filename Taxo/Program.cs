675ce0a89e18f194b6256632a81331b964af0ea6
Chig Beef <rhysharris@students.tyndale.sa.edu.au>
2022-07-06 15:18:43 +0930

    Added uses

--- Taxo/Program.cs
+++ Taxo/Program.cs
@@ -10,6 +10,9 @@ using System.IO;
  * This will be a new programming language,
  * based almost entirely on the objective of a perfect syntax.
  * 
+ * The language will be compiled into C#,
+ * this will make it versatile in that it can be run with other programs,
+ * such as unity.
 */
 
 namespace Taxo

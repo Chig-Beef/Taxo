846aff0a5ef138c4e28f9d01749ff02a5301431c
Chig Beef <classicalgas3@gmail.com>
2022-07-06 14:53:55 +0930

    Add project files.

---
+++ Taxo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Taxo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+        }
+
+        public class Test
+        {
+            public Test()
+            {
+                Console.WriteLine("hello");
+            }
+        }
+    }
+}

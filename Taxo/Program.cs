ebe8f89bcea4b04d62da5690c0809129bb768691
Chig Beef <rhysharris@students.tyndale.sa.edu.au>
2022-07-14 13:24:58 +0930

    Adding semicolons has now been implemented. This is done by adding semicolons to any line that doesn't increase the next line's indent.

--- Taxo/Program.cs
+++ Taxo/Program.cs
@@ -20,11 +20,18 @@ namespace Taxo
     {
         private static int Total { get; set; }
         private static string FileName { get; set; }
+        private static Dictionary<string, string> Translations { get;} = new Dictionary<string, string>(2);
+        private static int[] Tabs { get; set; }
 
         static void Main(string[] args)
         {
             // This is used to make sure the file names are consistent between reading and writing.
             FileName = "code";
+
+            Translations["Out"] = "Console.WriteLine";
+            Translations["strs"] = "string[]";
+
+            Tabs = new int[50];
 
             // Takes the filename and reads the corresponding file into an array of strings.
             string[] reader = Read_File(FileName + ".txt");
@@ -43,6 +50,8 @@ namespace Taxo
             string line; // To hold the current line being read
             int count = 0; // To count how many elements there are
             string[] reader = new string[50]; // To hold the lines
+            bool tabbed;
+            int iter;
 
             using (StreamReader sr = File.OpenText(filename))
             {
@@ -50,8 +59,26 @@ namespace Taxo
                 while (line != null)
                 {
                     reader[count] = line;
+
+                    // Finds out how many tabs are at the start of each line
+                    tabbed = false;
+                    iter = 0;
+                    while (!tabbed)
+                    {
+                        if (line.Substring(iter * 4, 4) == "    ")
+                        {
+                            Tabs[count]++;
+                        }
+                        else
+                        {
+                            tabbed = true;
+                        }
+
+                        iter++;
+                    }
+
                     count++;
-                    Console.WriteLine(line);
+
                     line = sr.ReadLine();
                 }
             }
@@ -84,13 +111,25 @@ namespace Taxo
             }
         }
 
-        /*
-         * Processing steps split
-         */
+        //
+        // Processing steps split
+        //
 
         // Adds the semicolons to each line
         private static string[] Add_Semicolons(string[] input)
         {
+            for (int i = 0; i < Total; i++)
+            {
+                Console.WriteLine(Tabs[i]);
+                if (Tabs[i] < Tabs[i + 1])
+                {
+                    
+                }
+                else
+                {
+                    input[i] += ";";
+                }
+            }
             return input;
         }
 
@@ -123,12 +162,25 @@ namespace Taxo
                 output[i + 4] = "        " + input[i];
             }
 
+            Total += 5;
+
             return output;
         }
 
         // Renames certain methods that have a simpler name in the uncompiled language
         private static string[] Rename_Methods(string[] input)
         {
+            for (int i = 0; i < Total; i++)
+            {
+                foreach (KeyValuePair<string, string> entry in Translations)
+                {
+                    if (input[i].IndexOf(entry.Key) != -1)
+                    {
+                        input[i] = input[i].Replace(entry.Key, entry.Value);
+                    }
+                }
+            }
+
             return input;
         }
 
